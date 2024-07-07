b4aade15d5c000cd205b17f76960ebddd61dc8a7
haits <transachhai97@gmail.com>
2024-07-07 10:31:35 +0700

    theme

--- src/app/ng-zorro.module.ts
+++ src/app/ng-zorro.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 
 @NgModule({
   exports: [
     NzIconModule,
     NzButtonModule,
+    NzDatePickerModule,
   ],
 })
 export class NgZorroModule {}
